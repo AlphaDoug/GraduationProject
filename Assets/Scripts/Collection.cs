6b963255ff3199d40c77a0ee61396f0c7c572f98
Doug <519558938@qq.com>
2018-03-22 14:24:34 +0800

    第二次修改  场景路径移动  收集品基本功能实现

---
+++ Assets/Scripts/Collection.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collection : MonoBehaviour
+{
+    /// <summary>
+    /// 下一个收集物
+    /// </summary>
+    public GameObject nextCollection;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (nextCollection != null)
+            {
+                nextCollection.SetActive(true);
+                gameObject.SetActive(false);
+            }
+            else
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
