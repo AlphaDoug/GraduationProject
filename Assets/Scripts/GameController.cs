a7960a64a6a2b95afac217d4922903c3e16246a9
Doug <519558938@qq.com>
2018-03-26 22:55:57 +0800

    加载界面添加 流程接入

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
     public GameObject[] maskObjs;
+    /// <summary>
+    /// 下一个场景的index
+    /// </summary>
+    [SerializeField]
+    private int nextSceneIndex;
     private int currentIndex;
 	// Use this for initialization
 	void Start ()
@@ -26,11 +31,23 @@ public class GameController : MonoBehaviour
         if (currentIndex > maskObjs.Length - 1)
         {
             Debug.Log("当前没有任务组,此关卡应该结束,切换下一个场景.");
+            LoadScene(nextSceneIndex);
         }
         else
         {
             Debug.Log("当前还有任务组,显示下一个任务.");
             maskObjs[currentIndex].SetActive(true);
         }
+    }
+
+    public void LoadScene(int index)
+    {
+        var loadingPrefab = (GameObject)Resources.Load("Prefab/LoadingScene");
+        var loading = Instantiate(loadingPrefab) as GameObject;
+        loading.GetComponent<LoadingController>().loadingSceneIndex = index;
+        loading.transform.parent = GameObject.Find("Canvas").transform;
+        loading.GetComponent<RectTransform>().localPosition = Vector2.zero;
+        loading.GetComponent<RectTransform>().localScale = Vector2.one;
+        loading.GetComponent<RectTransform>().localRotation = new Quaternion();
     }
 }
---
+++ Assets/Scripts/LoadingController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingController : MonoBehaviour
+{
+    public GameObject imageLoading;
+    public int loadingSceneIndex;
+
+    private AsyncOperation async;
+
+    // Use this for initialization
+    void Start ()
+    {
+
+
+        StartCoroutine(loadScene());
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        imageLoading.transform.RotateAround(imageLoading.transform.position, imageLoading.transform.forward, -4f);
+    }
+
+    IEnumerator loadScene()
+    {
+        yield return new WaitForSeconds(3.0f);
+
+        //异步读取场景。
+        async = SceneManager.LoadSceneAsync(loadingSceneIndex);
+
+        //读取完毕后返回， 系统会自动进入场景
+        yield return async;
+    }
+}
