6ccd2a3ccfc04a6b4c24b05500ea22c588edb927
AlphaDoug <519558938@qq.com>
2018-03-28 12:06:11 +0800

    更换文件目录

---
+++ Assets/Scenes/DBTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DBTest : MonoBehaviour
+{
+    private OOFormArray mForm = null;
+    // Use this for initialization
+    void Start ()
+    {
+        if (mForm == null)
+        {
+            mForm = OOFormArray.ReadFromResources("Data/Tables/Table_0");
+        }
+
+        Debug.Log(mForm.GetString("Name", 2));
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
