09d779372d71db0d7ade768ad4b7a5971f88bb15
Doug <519558938@qq.com>
2018-04-19 17:57:54 +0800

    添加相关文件 测试多收集物功能

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public delegate void ReachOnePosition();
+    public static event ReachOnePosition ReachOnePositionEvent;
+
+    public int ID;
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            gameObject.SetActive(false);
+            ReachOnePositionEvent();
+        }
+    }
+}
