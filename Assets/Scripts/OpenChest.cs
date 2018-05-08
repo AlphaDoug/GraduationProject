9c52cef854c9927e52b9bb2d7a16fdd2c77ddc21
AlphaDoug <519558938@qq.com>
2018-05-08 13:53:19 +0800

    加入新宝箱资源

---
+++ Assets/Scripts/OpenChest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenChest : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] chestPosition;
+    private OOFormArray mFormTbCollection = null;
+    private List<TaskController.CollectionAttribute> collectionAttribute = new List<TaskController.CollectionAttribute>();
+
+    private void Awake()
+    {
+        if (mFormTbCollection == null)
+        {
+            mFormTbCollection = OOFormArray.ReadFromResources("Data/Tables/TbCollection");
+        }
+        for (int i = 1; i < mFormTbCollection.mRowCount; i++)
+        {
+            var m_collectionAttribute = mFormTbCollection.GetObject<TaskController.CollectionAttribute>(i);
+            collectionAttribute.Add(m_collectionAttribute);
+        }
+    }
+    // Use this for initialization
+    void Start ()
+    {
+        for (int i = 0; i < collectionAttribute.Count; i++)
+        {
+            var collectionPrefab = (GameObject)Resources.Load(collectionAttribute[i].Path);
+            var collection = Instantiate(collectionPrefab) as GameObject;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
