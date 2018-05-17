037a393dfd1d0f0e2f13c5199195cbb5744ddd46
Doug <519558938@qq.com>
2018-05-17 17:50:12 +0800

    增加多机器人生成功能 修复随机行走bug并完善此功能 增加机器人配置表TbRobotConfig 修改脚本编码为UNICODE

---
+++ Assets/Scripts/RobotCreator.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotCreator : MonoBehaviour
+{
+    public class RobotAttribute
+    {
+        public int RobotID;
+        public float DirectionDampTime;
+        public float WaitTimeInterval;
+        public float MoveTimeInterval;
+        public string PrefabPath;
+        public string StartPosition
+        {
+            set
+            {
+                float x = float.Parse(value.Split(',')[0]);
+                float y = float.Parse(value.Split(',')[1]);
+                float z = float.Parse(value.Split(',')[2]);
+                StartPositionVector3 = new Vector3(x, y, z);
+            }
+        }
+        public Vector3 StartPositionVector3;
+    }
+
+    private OOFormArray robotForm = null;
+    private List<RobotAttribute> robotAttributes = new List<RobotAttribute>();
+    private void Awake()
+    {
+        if (robotForm == null)
+        {
+            robotForm = OOFormArray.ReadFromResources("Data/Tables/TbRobotConfig");
+        }
+
+        for (int i = 1; i < robotForm.mRowCount; i++)
+        {
+            var oneRobotAttribute = robotForm.GetObject<RobotAttribute>(i);
+            robotAttributes.Add(oneRobotAttribute);
+        }
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        for (int i = 0; i < robotAttributes.Count; i++)
+        {
+            if (!CreateRobot(robotAttributes[i]))
+            {
+                Debug.LogError("机器人未生成,请检查TbRobotConfig,ID为" + robotAttributes[i].RobotID);
+            }         
+        }
+
+	}
+	
+    private bool CreateRobot(RobotAttribute robotAttribute)
+    {
+        if (robotAttribute == null)
+        {
+            return false;
+        }
+        try
+        {
+            GameObject robotRes = Resources.Load(robotAttribute.PrefabPath) as GameObject;
+            GameObject robotObject = Instantiate(robotRes) as GameObject;
+            robotObject.GetComponent<RobotAIController>().DirectionDampTime = robotAttribute.DirectionDampTime;
+            robotObject.GetComponent<RobotAIController>().waitTimeInterval = robotAttribute.WaitTimeInterval;
+            robotObject.GetComponent<RobotAIController>().moveTimeInterval = robotAttribute.MoveTimeInterval;
+            robotObject.transform.position = robotAttribute.StartPositionVector3;
+        }
+        catch (System.Exception)
+        {
+            return false;
+            throw;
+        }
+        return true;
+    }
+
+}
