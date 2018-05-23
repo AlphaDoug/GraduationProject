54727732713482db778e025c7fbadb15c64eb669
AlphaDoug <519558938@qq.com>
2018-05-23 13:28:56 +0800

    机器人修改 论文修改

---
+++ Assets/Scripts/HelloText.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloText : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        Invoke("SetDisable", 2.0f);
+	}
+	
+    private void SetDisable()
+    {
+        gameObject.SetActive(false);
+    }
+}
